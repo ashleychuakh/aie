a83530d328fdbf0fbca58e5b5dbc69104be4f570
Zane J. Chua <zane@webtailors.sg>
2016-07-03 23:19:25 +0800

    Footer and Main Page 90% Completed

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -3,48 +3,78 @@
  <div class="container">
    <div class="footer-main">
     <div class="row">
-      <div class="col l6 s12">
-        <div class="col s12 center">
-          <p><img src="/assets/img/logo-white.png" alt="Meggnify"></p>
-        </div>
-        <div class="col s12 m6 google-download">
-          <a href=""><img src="/assets/img/googleplaydownload.png"></a>
-        </div>
-        <div class="col s12 m6 apple-download">
-          <a href=""><img src="/assets/img/googleplaydownload.png"></a>
-        </div>
-      </div>
-      <div class="col l4 offset-l2 s12">
-        <p class="grey-text text-lighten-3">Company</p>
+      <div class="col s4">
         <ul>
-          <li><a class="grey-text text-lighten-3" href="#">Privacy Policy</a></li>
-          <li><a class="grey-text text-lighten-3" href="#">Terms Of Use</a></li>
-          <li><a class="grey-text text-lighten-3" href="#!">Contact Us</a></li>
-          <li><a class="grey-text text-lighten-3" href="#!">About Us</a></li>
-          <li><a class="grey-text text-lighten-3" href="#!">Partners</a></li>
-          <li><a class="grey-text text-lighten-3" href="#!">Media</a></li>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About Us</a></li>
+          <li><a href="#!">Services</a></li>
+          <li><a href="#!">Products</a></li>
+          <li><a href="#!">Faq</a></li>
+          <li><a href="#!">Contact</a></li>
+        </ul>
+      </div>
+      <div class="col s4">
+        <h5>AIE ENGINEERING SVC</h5>
+        <p>LADFSFDS</p>
+        <ul class="social">
+          <li><a href="#" style="padding-left: 15px; padding-right: 15px;"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+          <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+          <li><a href="#!"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+        </ul>
+      </div>
+      <div class="col s4">
+        <ul class="details">
+          <li>
+            <img src="/assets/img/location.png">
+            <div class="content">
+              <p>blah</p>
+              <p>blah</p>
+              <p>blah</p>
+            </div>
+          </li>
+          <li>
+            <img src="/assets/img/call.png">
+            <div class="content">
+              <p>blah</p>
+              <p>blah</p>
+              <p>blah</p>
+            </div>
+          </li>
+          <li>
+            <img src="/assets/img/email.png">
+            <div class="content">
+              <p>blah</p>
+              <p>blah</p>
+              <p>blah</p>
+            </div>
+          </li>
+          <li>
+            <img src="/assets/img/time.png">
+            <div class="content">
+              <p>blah</p>
+              <p>blah</p>
+              <p>blah</p>
+            </div>
+          </li>
         </ul>
       </div>
      </div>
    </div>
+   <div class="footer-copyright">
+     <div class="row">
+       <div class="col s6">
+        <ul>
+          <li><a href="">Contact Us</a></li>
+          <li><a href="">Privacy Policy</a></li>
+          <li><a href="">Terms & Conditions</a></li>
+        </ul>
+       </div>
+       <div class="col s6 right">
+        <p>© 2016 AIE Engineering SVC. All Rights Reserved</p>
+       </div>
+     </div>
+   </div>
  </div>
- <div class="footer-copyright">
-  <div class="row">
-    <div class="col s12">
-        <div class="footer-follow">
-          <p>FOLLOW US ON</p>
-          <ul>
-            <a href=""><li><i class="fa fa-facebook" aria-hidden="true"></li></i></a>
-            <a href=""><li><i class="fa fa-twitter" aria-hidden="true"></li></i></a>
-            <a href=""><li><i class="fa fa-instagram" aria-hidden="true"></li></i></a>
-          </ul>
-          <p>© 2016 MEGGNIFY PTE LTD</p>
-        </div>
-        <div class="footer-totop">
-          <a id="return-to-top" href="javascript:;"><div class="totop-link"><i class="fa fa-angle-up" aria-hidden="true"></i></div></a>
-        </div>
-    </div>
-  </div>
- </div>
+
 </footer>
 @show
