d265cbc4b19ae30eb3ec808d791c54e47890bad9
Zane J. Chua <zane@webtailors.sg>
2016-07-03 20:55:14 +0800

    Main Page Design and Header

--- resources/views/pages/main.blade.php
+++ resources/views/pages/main.blade.php
@@ -3,17 +3,8 @@
 @section("head")
 <title>Meggnify</title>
 <style>
-
-.hero {
-  color: #fff;
-  background-color: #000;
-  min-height: 400px;
-  padding-top: 20px;
-  padding-bottom: 20px
-}
-.hero p {
-  color: #fff;
-  font-size: 1.5em;
+header {
+  z-index: 1000;
 }
 .grey-section {
   background-color: #bdbdbd;
@@ -26,23 +17,181 @@
   min-height: 400px;
   padding-bottom: 20px
 }
+.hero {
+  color: #fff;
+  min-height: 720px;
+  position: relative;
+  bottom: 0%;
+  left: 0%;
+  height: 100%;
+  width: 100%;
+  overflow: hidden;
+  background: #000;
+  margin-top: -190px;
+}
+
+.hero p {
+  color: #fff;
+  font-size: 1.5em;
+}
+
+.hero-text {
+  position: absolute;
+  text-align: center;
+  z-index: 2;
+  width: 100%;
+  margin-top: 450px;
+}
+
+.hero .owl-stage-outer {
+  z-index: -1;
+}
+.hero .owl-dots {
+    position: absolute;
+    z-index: 1;
+    right: 0;
+    margin-right: 50px;
+    bottom: 0;
+    margin-bottom: 30px;
+}
+
+.hero .owl-theme .owl-dots .owl-dot span {
+  background: #676767;
+  margin: 5px 2.5px;
+}
+
+.hero .owl-theme .owl-dots .owl-dot.active span, .hero .owl-theme .owl-dots .owl-dot:hover span {
+  background: #3D9FF9;
+}
+
+.hero-item-blank {
+  background: #fff;
+  background-position: center;
+  text-align: center;
+  padding: 20px;
+}
+
+.hero-item-blank .item-text {
+  text-align: center;
+}
+
+.hero-item-blank .owl-stage-outer {
+  z-index: -1;
+}
+.hero-item-blank .owl-dots {
+    position: absolute;
+    z-index: 1;
+    right: 0;
+    margin-right: 50px;
+    bottom: 0;
+    margin-bottom: 30px;
+}
+
+.hero-item-blank .owl-theme .owl-dots .owl-dot span {
+  background: #676767;
+  margin: 5px 2.5px;
+}
+
+.hero-item-blank .owl-theme .owl-dots .owl-dot.active span, .hero-item-blank .owl-theme .owl-dots .owl-dot:hover span {
+  background: #3D9FF9;
+}
+
+.hero-item-blank .owl-carousel .owl-item img {
+  width: 72px;
+  height: 72px;
+  margin: 0 auto;
+}
+
+.hero-item-1 {
+  background: linear-gradient(rgba(0,0,0,0.05),rgba(0,0,0,0.05)),url(/assets/img/main/hero-item-1.jpg);
+  min-height: 600px;
+  background-position: center;
+  text-align: center;
+}
+
+.hero-item-2 {
+  background: linear-gradient(rgba(0,0,0,0.05),rgba(0,0,0,0.05)),url(/assets/img/main/hero-item-2.jpg);
+  min-height: 600px;
+  background-position: center;
+  text-align: center;
+}
 </style>
+<link type="text/css" rel="stylesheet" href="/assets/css/owl.carousel.css" />
+<link type="text/css" rel="stylesheet" href="/assets/css/owl.theme.default.min.css" />
 @stop
 
 @section("content")
   <main>
-    <div class="container">
-      <div class="row">
-        <div class="col s12">
+    <div class="hero">
+      <div class="hero-text">
+        <h1>Lorem ipsum dolor</h1>
+        <p>afasdfasfasfdasfdasfasdfasdfsfsafasdf</p>
+      </div>
+      <div id="owl-hero" class="owl-carousel owl-theme">
+        <div class="item"><img src="/assets/img/main/01.jpg"></div>
+        <div class="item"><img src="/assets/img/main/02.jpg"></div>
+        <div class="item"><img src="/assets/img/main/03.jpg"></div>
+      </div>
+    </div>
+    <div class="hero-item-blank">
+      <div class="container">
+        <div class="item-text">
+          <h3>Our Services</h3>
+          <p>Only the Highest Quality of Services For You</p>
+        </div>
+
+        <div id="owl-item" class="owl-carousel owl-theme">
+          <div class="item">
+            <img alt="General Service" src="/assets/img/main/snowflake.png" width="72px">
+            <h5>General Service</h5>
+            <p>blahblahblah</p>
+          </div>
+          <div class="item">
+            
+          </div>
+          <div class="item">
+            
+          </div>
+          <div class="item">
+            
+          </div>
+          <div class="item">
+            
+          </div>
+          <div class="item">
+            
+          </div>
         </div>
       </div>
+    </div>
+    <div class="hero-item-1">
+    asdfsf
+    </div>
+    <div class="hero-item-2">
+    asdfsf
     </div>
   </main>
 @stop
 
 @section("scripts")
+  <script src="/assets/js/owl.carousel.js"></script>
   <script>
     $(function() {
+      $('#owl-hero').owlCarousel({
+          items:1,
+          margin:10,
+          loop: true,
+          dots: true,
+          autoplay: true
+      });
+
+      $('#owl-item').owlCarousel({
+          items:3,
+          margin:10,
+          loop: true,
+          dots: true,
+          autoplay: false
+      });
     });
   </script>
 @stop
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,43 +1,62 @@
 @section("header")
-<div class="navbar">
-  <nav>
-    <div class="row">
-      <div class="col l12">
-        <div class="nav-wrapper">
-          <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
-          <a href="{{ route('main') }}" class="logo"><img src="/assets/img/logo.png" alt="Meggnify"></a>
-          <ul class="left">
+<header>
+  <div class="top-bar no-shadow">
+    <div class="container">
+      <div class="row no-margin">
+        <div class="col s6">
+          <ul>
+            <li><a href="">(65) 6592 6887</a></li>
+            <li><a href="">Operating Hours: Mon-Sat 11am-6pm</a></li>
           </ul>
-          
-          <ul class="right hide-on-med-and-down">
-            @if(Auth::check())
-              <li><a href="#">ASSIGNMENT</a></li>
-              <li><a class="waves-effect waves-dark btn-link btn dropdown-button" href="javascript:;" data-activates="dropdown1">My Account<i class="material-icons right" style="line-height: 35px;">arrow_drop_down</i></a></li>
-              <ul id="dropdown1" class="dropdown-content" style="margin-left: 15px;">
-                 @if(Auth::user()->type == 0)<li><a href="{{ route('admin/dashboard') }}">Administrative Dashboard</a></li>@endif
-                <li><a href="{{ route('signout') }}">Sign Out</a></li>
-              </ul>
-            @else
-              <li><a href="{{ route('signin') }}">Sign In</a></li>
-              <li><a href="{{ route('signup') }}">Sign Up</a></li>
-            @endif
-          </ul>
-          <ul class="side-nav" id="mobile-demo">
-            @if(Auth::check())
-              <li><a href="#">ASSIGNMENT</a></li>
-              <li><a class="waves-effect waves-dark btn-link btn dropdown-button" href="javascript:;" data-activates="dropdown2">My Account<i class="material-icons right" style="line-height: 64px;">arrow_drop_down</i></a></li>
-              <ul id="dropdown2" class="dropdown-content" style="margin-left: 10px; margin-top: 15px;">
-                 @if(Auth::user()->type == 0)<li><a href="{{ route('admin/dashboard') }}">Administrative Dashboard</a></li>@endif
-                <li><a href="{{ route('signout') }}">Sign Out</a></li>
-              </ul>
-            @else
-              <li><a href="{{ route('signin') }}">Sign In</a></li>
-              <li><a href="{{ route('signup') }}">Sign Up</a></li>
-            @endif
+        </div>
+        <div class="col s6 right">
+          <ul>
+            <li><a href="" class="btn-border">BOOK NOW</a></li>
+            <li><a href="">Login</a></li>
+            <li><div class="verticalline"></div></li>
+            <li><a href="">Signup</a></li>
           </ul>
         </div>
       </div>
     </div>
-  </nav>
-</div>
+  </div>
+  <div class="navbar">
+    <nav class="no-shadow bottom-shadow">
+      <div class="container">
+        <div class="row">
+          <div class="col l12">
+            <div class="nav-wrapper">
+              <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+              <a href="{{ route('main') }}" class="logo"><img src="/assets/img/logo.png" alt="Meggnify"></a>
+              <ul class="left">
+              </ul>
+              
+              <ul class="right hide-on-med-and-down">
+                  <li class="active"><a href="{{ route('main') }}">Home</a></li>
+                  <li><a href="{{ route('signin') }}">About Us</a></li>
+                  <li><a href="{{ route('signup') }}">Services</a></li>
+                  <li><a href="{{ route('signup') }}">Products</a></li>
+                  <li><a href="{{ route('signup') }}">FAQ</a></li>
+                  <li><a href="{{ route('signup') }}">Contact</a></li>
+              </ul>
+              <ul class="side-nav" id="mobile-demo">
+                @if(Auth::check())
+                  <li><a href="#">ASSIGNMENT</a></li>
+                  <li><a class="waves-effect waves-dark btn-link btn dropdown-button" href="javascript:;" data-activates="dropdown2">My Account<i class="material-icons right" style="line-height: 64px;">arrow_drop_down</i></a></li>
+                  <ul id="dropdown2" class="dropdown-content" style="margin-left: 10px; margin-top: 15px;">
+                     @if(Auth::user()->type == 0)<li><a href="{{ route('admin/dashboard') }}">Administrative Dashboard</a></li>@endif
+                    <li><a href="{{ route('signout') }}">Sign Out</a></li>
+                  </ul>
+                @else
+                  <li><a href="{{ route('signin') }}">Sign In</a></li>
+                  <li><a href="{{ route('signup') }}">Sign Up</a></li>
+                @endif
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </div>
+</header>
 @show
